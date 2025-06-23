a132c65dee73082e2e6525278263d878f7114af2
Bart van Stratum <bart@vanstratum.com>
2025-06-23 14:57:12 +0200

    Added functionality to read/write `.ini` files.

--- microhhpy/io/__init__.py
+++ microhhpy/io/__init__.py
@@ -1,3 +1,4 @@
 from .netcdf_io import xr_open_groups
+from .ini_io import read_ini, save_ini, check_ini
 
-__all__ = ['xr_open_groups']
+__all__ = ['xr_open_groups', 'read_ini', 'save_ini', 'check_ini']
