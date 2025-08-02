fbc59596c74dd6d57f8c1b9a7f8aa1264d079c30
Bart van Stratum <bart@vanstratum.com>
2025-08-02 14:34:30 +0200

    Added function to parse `.out` files from MicroHH.

--- microhhpy/io/__init__.py
+++ microhhpy/io/__init__.py
@@ -1,4 +1,5 @@
 from .netcdf_io import xr_open_groups
 from .ini_io import read_ini, save_ini, check_ini
+from .read_out import read_case_out
 
-__all__ = ['xr_open_groups', 'read_ini', 'save_ini', 'check_ini']
+__all__ = ['xr_open_groups', 'read_ini', 'save_ini', 'check_ini', 'read_case_out']
