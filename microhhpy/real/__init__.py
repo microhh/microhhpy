cad321c3d9fbe9df658b8e1f42037e2a4f5d841c
Bart van Stratum <bart@vanstratum.com>
2025-08-15 08:14:09 +0200

    Added function to init SSTs from ERA5.

--- microhhpy/real/__init__.py
+++ microhhpy/real/__init__.py
@@ -1,6 +1,10 @@
 from .input_from_regular_latlon import create_era5_input, create_input_from_regular_latlon
 from .lbc_help_functions import create_lbc_ds, lbc_ds_to_binary
+from .sea_input import create_sst_from_regular_latlon
 
 __all__ = [
-        'create_era5_input', 'create_input_from_regular_latlon', 
-        'create_lbc_ds', 'lbc_ds_to_binary']
+        'create_era5_input',
+        'create_input_from_regular_latlon', 
+        'create_lbc_ds',
+        'lbc_ds_to_binary',
+        'create_sst_from_regular_latlon']
