aa7a62270a0348fe4683730891b8e23230e791d9
Bart van Stratum <bart@vanstratum.com>
2025-08-23 10:33:05 +0200

    Update MicroHH constants.

--- microhhpy/constants.py
+++ microhhpy/constants.py
@@ -20,13 +20,23 @@
 #  along with MicroHH.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-Rd = 287.04
-Rv = 461.5
-Lv = 2.501e6
-Lf = 3.337e5
-Ls = Lv+Lf
-cp = 1005.
-p0 = 1.e5
-ep = Rd/Rv
-T0 = 273.15
-grav = 9.81
+kappa = 0.4           # von Karman constant (-)
+grav = 9.81           # Gravitational acceleration (m s-2)
+e_rot = 7.2921e-5     # Earth rotation rate (s-1)
+Rd = 287.04           # Gas constant for dry air (J K-1 kg-1)
+Rv = 461.5            # Gas constant for water vapor (J K-1 kg-1)
+cp = 1005             # Specific heat of air at constant pressure (J kg-1 K-1)
+Lv = 2.501e6          # Latent heat of vaporization (J kg-1)
+Lf = 3.337e5          # Latent heat of fusion (J kg-1)
+Ls = Lv + Lf          # Latent heat of sublimation (J kg-1)
+T0 = 273.15           # Freezing / melting temperature (K)
+p0 = 1.e5             # Reference pressure (Pa)
+rho_w = 1.e3          # Density of water (kg m-3)
+rho_i = 7.e2          # Density of ice   (kg m-3)
+mu0_min = 1e-6        # Minimum value used for cos(sza)
+sigma_b = 5.67e-8     # Boltzmann constant (W m-1 K-1)
+ep = Rd / Rv          # Ratio gas constants dry air and water vapor (-)
+
+mm_air = 28.9647      # Molar mass of dry air (kg kmol-1)
+mm_h2o = 18.01528     # Molar mass of H2O (kg kmol-1)
+mm_co2 = 44.0095      # Molar mass of CO2 (kg kmol-1)
